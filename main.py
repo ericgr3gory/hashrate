f563089b046b7fda5f90c2a58e85f3a80a694223
ericgr3gory <eric@e-gregory.com>
2024-01-16 17:37:23 -0800

    new

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
 from apikey import api_key
 import requests
 import json
+import argparse
+import sys
 
-
-def gpu_data():
-    req = requests.get(f"https://api.hashrate.no/v1/gpuEstimates?apiKey={api_key}&powerCost=0.17")
+def hash_data(power, compute):
+    req = requests.get(f"https://api.hashrate.no/v1/{compute}Estimates?apiKey={api_key}&powerCost={power}")
+    write(req.json())
     return req.json()
 
-def write(gpu_json):
+def write(hash_json):
     with open(file='hashrate', mode="w") as j:
-        gpu_json = json.dumps(gpu_json)
-        j.write(gpu_json)
+        hash_json = json.dumps(hash_json)
+        j.write(hash_json)
 def read():
     with open(file='hashrate', mode='r') as r:
         return json.load(r)
 
+def check_for_file():
+
+    try:
+        return read()
+
+    except FileNotFoundError:
+        return False
+
+def display_results(json_data, devices):
+
+    for i in json_data:
+        if i in devices:
+            print(f'{i} {json_data[i]["profit"]["coin"]} profit: {json_data[i]["profit"]["profitUSD"]}, revenue: {json_data[i]["profit"]["revenueUSD"]}')
+            print(f'{i} {json_data[i]["revenue"]["coin"]} revenue: {json_data[i]["revenue"]["revenueUSD"]} profit: {json_data[i]["revenue"]["profitUSD"]}')
+
+
+def list_compute_devices(json_data):
+    for i in json_data:
+        print(i)
 
 def main():
-    try:
-        gpu_json_data = read()
-    except FileNotFoundError:
-        gpu_data_json = gpu_data()
-        write(gpu_data_json)
+    if args.reload:
+        hashrateno_json_data = hash_data(args.power, args.type)
+    else:
+        hashrateno_json_data = read()
 
-    for i in gpu_json_data:
-        print(i['device'])
+    if args.list_devices:
+        list_compute_devices(read())
+        sys.exit()
 
-
-
+    display_results(hashrateno_json_data, args.devices)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='HashRate.no\'s api tools.. get coins that are most profitable and highest revenue for your devices')
+    parser.add_argument('--reload', help='reload data from api', action="store_true")
+    parser.add_argument('--type', help='compute type gpu or cpu default is gpu', type=str, default="gpu")
+    parser.add_argument('--power', help='cost of power in kWh default is 0.18 USD', type=str, default="0.18")
+    parser.add_argument('--list_devices', help='list devices, when combines with --type available from api', action="store_true")
+    parser.add_argument('--devices', help='return data for these devices',  nargs='+', type=str, default=["5700g","3060", "1660s", "2060s"])
+    args = parser.parse_args()
     main()
